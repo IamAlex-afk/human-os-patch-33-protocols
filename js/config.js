04f1fc9307d177b9521b6812d92952f21fe8eb34
Aleksei Sergeevich Bitkin (AI Biohacking) <sintyau1777@gmail.com>
2026-05-06 02:22:03 +0700

    Update POLL_API_URL to new endpoint

--- js/config.js
+++ js/config.js
@@ -4,7 +4,8 @@ const CONFIG = {
   MAX_AXIS: 32,
   TOTAL_MAX: 96,
   FEAR_MAX: 16,
-  POLL_API_URL: "https://script.google.com/macros/s/AKfycbxCL8wxUhHPGLQyhAhT7B5rBCSjKnRLq2oO7TApvFJlaYC8oVOwTNcrwR3wa5JeYmv18A/exec",
+  // ОБНОВЛЕННАЯ ССЫЛКА (Версия 3)
+  POLL_API_URL: "https://script.google.com/macros/s/AKfycbxze15u0M-8LrpJuwSVMAIbAGovp_cB9auqW6rsDY_lr_ZM5iiMjSr2__7RzJcD3p6S7A/exec",
   STORAGE_KEYS: {
     TRACKER: 'mindos_tracker_v3',
     POLL_VOTED: 'mindos_poll_v3_voted',
